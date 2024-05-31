5ba1eeeb7e4d3609e5eba423db2341fb38869931
Tair Dospayev <tdospayev@klika-tech.com>
2024-05-31 18:03:25 +0500

    added useTelegram.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,29 @@
-// src/App.jsx
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import {useTelegram} from "./hooks/useTelegram";
 
 function App() {
-    const [menu, setMenu] = useState([]);
-    const [cart, setCart] = useState([]);
+  const {tg, user, onToggleButton} = useTelegram();
 
-    useEffect(() => {
-        axios.get('http://localhost:3000/menu')
-            .then(response => setMenu(response.data))
-            .catch(error => console.error('Error fetching menu:', error));
-    }, []);
+  useEffect(() => {
+    tg.ready();
+  }, []);
+  
+  console.log("user", user);  
 
-    const addToCart = (item) => {
-        setCart([...cart, item]);
-    };
+  const onClose = () => {
+    tg.close();
+  }
 
-    const removeFromCart = (item) => {
-        setCart(cart.filter(cartItem => cartItem.id !== item.id));
-    };
-
-    const handleOrder = () => {
-        axios.post('http://localhost:3000/order', { items: cart })
-            .then(response => alert('Order placed successfully!'))
-            .catch(error => console.error('Error placing order:', error));
-    };
-
-    return (
-        <div className="App">
-            <h1>Food Ordering Miniapp</h1>
-            <h2>Menu1</h2>
-            <ul>
-                {menu.map(item => (
-                    <li key={item.id}>
-                        {item.name} - ${item.price}
-                        <button onClick={() => addToCart(item)}>Add to Cart</button>
-                    </li>
-                ))}
-            </ul>
-            <h2>Cart</h2>
-            <ul>
-                {cart.map(item => (
-                    <li key={item.id}>
-                        {item.name}
-                        <button onClick={() => removeFromCart(item)}>Remove</button>
-                    </li>
-                ))}
-            </ul>
-            <button onClick={handleOrder}>Place Order</button>
-        </div>
-    );
+  return (
+      <div className="App">
+          <h1>Food Ordering Miniapp</h1>
+          <h2>Menu1</h2>
+          <button onClick={onClose}>Close</button>
+          <button onClick={onToggleButton}>toggle</button>
+      </div>
+  );
 }
 
 export default App;
