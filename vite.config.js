63965834dc9f42208588c0260785380731b19d50
Tair Dospayev <tdospayev@klika-tech.com>
2024-05-31 16:57:22 +0500

    updated define config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        https: {
-            key: fs.readFileSync('ssl/server.key'),
-            cert: fs.readFileSync('ssl/server.cert'),
-        },
-        host: 'food.delivery',
-        port: 5173,
-    },
+  plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 5173,
+  },
 });
