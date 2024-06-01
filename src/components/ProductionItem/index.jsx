6d14a75ac1f9a2f7c26f5167d98b63d86ae20802
Dospayev Tair <tair.dospayev@gmail.com>
2024-06-01 15:41:32 +0500

    updated product item css

--- src/components/ProductionItem/index.jsx
+++ src/components/ProductionItem/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from "../Button";
-import './ProductionItem.css';
 
 const ProductItem = ({product, className, onAdd}) => {
 
@@ -9,14 +8,17 @@ const ProductItem = ({product, className, onAdd}) => {
 	};
 
 	return (
-		<div className={'product ' + className}>
-			<div className={'img'}/>
-			<div className={'title'}>{product.title}</div>
-			<div className={'description'}>{product.description}</div>
-			<div className={'price'}>
+		<div className={`product ${className} max-w-sm rounded overflow-hidden shadow-lg bg-white p-4`}>
+			<div className="img h-48 bg-gray-200 flex items-center justify-center">
+				<img src={product.image} alt={product.title} className="h-full w-full object-cover"/>
+			</div>
+			<div className="title font-bold text-xl mt-4">{product.title}</div>
+			<div className="description text-gray-700 text-base mt-2">{product.description}</div>
+			<div className="price text-lg mt-4">
 				<span>Стоимость: <b>{product.price}</b></span>
 			</div>
-			<Button className={'add-btn'} onClick={onAddHandler}>
+			<Button className="add-btn mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			        onClick={onAddHandler}>
 				Добавить в корзину
 			</Button>
 		</div>
