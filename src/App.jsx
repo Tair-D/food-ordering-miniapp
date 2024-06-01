21a93804d9942f1b288bd29e41d9e17fda64374a
Dospayev Tair <tair.dospayev@gmail.com>
2024-06-01 09:15:52 +0500

    added the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import React, {useEffect} from 'react';
 import './App.css';
+import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header";
+import {Route, Routes} from 'react-router-dom';
+import ProductList from "./pages/ProductList/index.jsx";
+import Registration from "./pages/Registration/index.jsx";
 
 function App() {
-	const {tg, user, onToggleButton} = useTelegram();
+	const {onToggleButton, tg} = useTelegram();
 
 	useEffect(() => {
 		tg.ready();
 	}, []);
 
-	console.log("user", user);
-
-	const onClose = () => {
-		tg.close();
-	};
-
 	return (
-		<div className="min-h-screen bg-gray-100 p-4">
-			<div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg p-6">
-				<h1 className="text-2xl font-bold mb-4">Food Ordering Miniapp</h1>
-				<h2 className="text-xl font-semibold mb-2">Menu</h2>
-
-				<h2 className="text-xl font-semibold mb-2">Cart</h2>
-				<button
-					className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
-					onClick={onClose}
-				>
-					Close
-				</button>
-			</div>
+		<div className="App">
+			<Header/>
+			<Routes>
+				<Route index element={<ProductList/>}/>
+				<Route path={'form'} element={<Registration/>}/>
+			</Routes>
 		</div>
 	);
 }
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -2,22 +2,23 @@ const tg = window.Telegram.WebApp;
 
 export const useTelegram = () => {
 	const onClose = () => {
-		tg.close()
-	}
-	
+		tg.close();
+	};
+
 	const onToggleButton = () => {
 		if (tg.MainButton.isVisible) {
 			tg.MainButton.hide();
 		} else {
 			tg.MainButton.show();
 		}
-	}
-	
-	
+	};
+
+
 	return {
 		onClose,
 		onToggleButton,
 		tg,
-		user: tg.initDataUnsafe?.user
-	}
-}
+		user: tg.initDataUnsafe?.user,
+		queryId: tg.initDataUnsafe?.query_id,
+	};
+};
