843e8d5c1536df7d5ba8493d4b24ae49bc42ce78
Dospayev Tair <tair.dospayev@gmail.com>
2024-06-02 14:57:40 +0500

    updated product item

--- src/components/ProductionItem/index.jsx
+++ src/components/ProductionItem/index.jsx
@@ -8,19 +8,22 @@ const ProductItem = ({product, className, onAdd}) => {
 		onAdd(product);
 	};
 
+	const truncateDescription = (description) => {
+		return description.length > 100 ? description.substring(0, 100) + '...' : description;
+	};
+
 	return (
 		<div className={`product ${className}`}>
 			<div className="img">
 				<img src={product.image} alt={product.title} className="img"/>
 			</div>
 			<div className="title">{product.title}</div>
-			<div className="description">{product.description}</div>
+			<div className="description">{truncateDescription(product.description)}</div>
 			<div className="price">
 				<span>Стоимость: <b>{product.price}</b></span>
 			</div>
-			<Button className="add-btn"
-			        onClick={onAddHandler}>
-				`{product.price}` Добавить в корзину
+			<Button className="add-btn" onClick={onAddHandler}>
+				<span className="add-icon">+</span> Добавить в корзину
 			</Button>
 		</div>
 	);
