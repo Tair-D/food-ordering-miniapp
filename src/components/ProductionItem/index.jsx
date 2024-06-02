07d889e2e9ad180a69ae037a24dcfb20b4b6bf81
Dospayev Tair <tair.dospayev@gmail.com>
2024-06-02 15:20:05 +0500

    updated add

--- src/components/ProductionItem/index.jsx
+++ src/components/ProductionItem/index.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "../Button";
 import "./ProductionItem.css";
 
 const ProductItem = ({product, className, onAdd}) => {
+	const [count, setCount] = useState(0);
 
 	const onAddHandler = () => {
+		setCount(count + 1);
 		onAdd(product);
 	};
 
+	const onIncrementHandler = () => {
+		setCount(count + 1);
+		onAdd(product);
+	};
+
+	const onDecrementHandler = () => {
+		if (count > 1) {
+			setCount(count - 1);
+		} else {
+			setCount(0);
+		}
+	};
+
 	const truncateDescription = (description) => {
-		return description.length > 30 ? description.substring(0, 30) + '...' : description;
+		return description.length > 100 ? description.substring(0, 100) + '...' : description;
 	};
 
 	return (
@@ -19,12 +34,20 @@ const ProductItem = ({product, className, onAdd}) => {
 			</div>
 			<div className="title">{product.title}</div>
 			<div className="description">{truncateDescription(product.description)}</div>
-			{/*<div className="price">*/}
-			{/*	<span>Стоимость: <b>{product.price}</b></span>*/}
-			{/*</div>*/}
-			<Button className="add-btn" onClick={onAddHandler}>
-				<span className="price-text">{product.price} ₸</span> <span className="add-icon">+</span>
-			</Button>
+			<div className="price">
+				<span>Стоимость: <b>{product.price}</b></span>
+			</div>
+			{count === 0 ? (
+				<Button className="add-btn" onClick={onAddHandler}>
+					<span className="price-text">{product.price} ₸</span> <span className="add-icon">+</span>
+				</Button>
+			) : (
+				<div className="quantity-controls">
+					<Button className="decrement-btn" onClick={onDecrementHandler}>-</Button>
+					<span className="item-count">{count}</span>
+					<Button className="increment-btn" onClick={onIncrementHandler}>+</Button>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/pages/ProductList/index.jsx
+++ src/pages/ProductList/index.jsx
@@ -95,14 +95,14 @@ const ProductList = () => {
 	}, [onSendData]);
 
 	const onAdd = (product) => {
-		const alreadyAdded = addedItems.find(item => item.id === product.id);
+		// const alreadyAdded = addedItems.find(item => item.id === product.id);
 		let newItems = [];
 
-		if (alreadyAdded) {
-			newItems = addedItems.filter(item => item.id !== product.id);
-		} else {
-			newItems = [...addedItems, product];
-		}
+		// if (alreadyAdded) {
+		// 	newItems = addedItems.filter(item => item.id !== product.id);
+		// } else {
+		// }
+		newItems = [...addedItems, product];
 
 		setAddedItems(newItems);
 
