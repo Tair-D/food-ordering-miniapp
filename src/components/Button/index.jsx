82496e32f94f15a4fa6d9e7b12761c406c432c00
Dospayev Tair <tair.dospayev@gmail.com>
2024-06-02 13:52:41 +0500

    updated button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,7 +3,8 @@ import './button.css';
 
 const Button = (props) => {
 	return (
-		<button {...props} className={`btn ${props.className} button`}/>
+		<button {...props}
+		        className={`btn ${props.className} bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}/>
 	);
 };
 
