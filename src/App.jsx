770087521799011812aeda3f0326f2b00c323979
Tair Dospayev <tdospayev@klika-tech.com>
2024-06-06 12:27:51 +0500

    added redirects

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,6 @@ import Registration from "./pages/Registration/index.jsx";
 import ConfirmationPage from "./pages/Confirmation/index.jsx";
 import ShippingInfo from "./pages/ShippingInfo/index.jsx";
 
-const test = [
-	{
-		"id": "2",
-		"title": "Куртка",
-		"price": 12000,
-		"description": "Зеленого цвета, теплая",
-		"image": "https://i.pinimg.com/originals/6f/5f/53/6f5f5332cd54ba419022a4882935dbd5.png",
-		"count": 1
-	},
-	{
-		"id": "1",
-		"title": "Джинсы",
-		"price": 5000,
-		"description": "Синего цвета, прямые,Синего цвета, прямые,Синего цвета, прямые,Синего цвета, прямые,Синего цвета, прямые",
-		"image": "https://i.pinimg.com/originals/6f/5f/53/6f5f5332cd54ba419022a4882935dbd5.png",
-		"count": 1
-	}
-];
-
 function App() {
 	const {tg} = useTelegram();
 
